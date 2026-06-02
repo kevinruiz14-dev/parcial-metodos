aec9d09a93e569c111e7cc91f3cf3797f93c338a
Gabrielaaaa <andinogabriela79@gmail.com>
2026-06-01 19:50:49 -0600

    primer commit: menú principal

---
+++ main.py
@@ -0,0 +1,8 @@
+print("=== SISTEMA DE REGRESIÓN NUMÉRICA ===")
+print("[1] Regresión Lineal")
+print("[2] Regresión Cuadrática")
+print("[3] Regresión Exponencial")
+print("[0] Salir")
+
+opcion = input("Elige una opción: ")
+print(f"Elegiste: {opcion}")
